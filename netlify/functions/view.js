5fc5929e6e780fda5e6ee267e5900a4218124b56
Jerry03Tom22 <princekuzuri4400@gmail.com>
2026-02-19 15:48:02 +0530

    Update view.js

--- netlify/functions/view.js
+++ netlify/functions/view.js
@@ -1,32 +1,56 @@
-<script>
-const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
+const admin = require("firebase-admin");
 
-fetch(`/.netlify/functions/view?id=${id}`)
-  .then(async res => {
-
-    if (!res.ok) {
-      const text = await res.text();
-      document.body.innerHTML = text;
-      return null;
-    }
-
-    return res.json();
-  })
-  .then(data => {
-
-    if (!data) return;
-
-    if(data.youtubeId){
-      document.getElementById("player").innerHTML =
-        `<iframe width="560" height="315"
-         src="https://www.youtube.com/embed/${data.youtubeId}"
-         frameborder="0"
-         allowfullscreen></iframe>`;
-    }
-
-  })
-  .catch(err => {
-    document.body.innerHTML = "Something went wrong";
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    })
   });
-</script>
+}
+
+const db = admin.firestore();
+
+exports.handler = async (event) => {
+  try {
+    const id = event.queryStringParameters.id;
+
+    const doc = await db.collection("links").doc(id).get();
+
+    if (!doc.exists) {
+      return { statusCode: 404, body: "Link not found" };
+    }
+
+    const data = doc.data();
+
+    if (Date.now() > data.expiry) {
+      return { statusCode: 403, body: "Link expired" };
+    }
+
+    const url = data.youtubeUrl;
+
+    let youtubeId = null;
+
+    if (url.includes("v=")) {
+      youtubeId = url.split("v=")[1].split("&")[0];
+    } else if (url.includes("youtu.be/")) {
+      youtubeId = url.split("youtu.be/")[1].split("?")[0];
+    }
+
+    if (!youtubeId) {
+      return { statusCode: 400, body: "Invalid YouTube URL" };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ youtubeId })
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.toString()
+    };
+  }
+};
